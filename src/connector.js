98710a3a3fe10f699b77f21baed2da96f95a5882
madanagula-3 <madanagula3@gmail.com>
2023-04-26 11:39:09 +0530

    commited

---
+++ src/connector.js
@@ -0,0 +1,18 @@
+
+//const mongodb = require('mongodb');
+
+const mongoURI = "mongodb+srv://pavanagulla19:PAVAN123@cluster0.fsjcdoy.mongodb.net/test"
+
+let mongoose = require('mongoose');
+const { tallySchema } = require('./schema')
+
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { console.log("connection established with mongodb server online"); })
+    .catch(err => {
+        console.log("error while connection", err)
+    });
+collection_connection = mongoose.model('covidtally', tallySchema)
+
+
+exports.connection = collection_connection;
